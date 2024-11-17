d12cd0c302c448340578580255b86ec27424ab46
hainguyen-eh <nguyenngochai.shipagent@gmail.com>
2024-11-17 20:40:04 +0700

    Update config path

--- lib/vietnam_address/configuration.rb
+++ lib/vietnam_address/configuration.rb
@@ -2,8 +2,8 @@ module VietnamAddress
   class Configuration
     attr_accessor :data_path
 
-    def initialize
-      @data_path = Gem.loaded_specs['vietnam_address'].gem_dir + '/data'
+    def initialize(data_path: nil)
+      @data_path = data_path || Gem.loaded_specs['vietnam_address'].gem_dir + '/data'
     end
   end
 end
